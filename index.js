5500362029be5a662fd8cb9d515120a1133f4a14
aristo85 <aritomsk@yahoo.com>
2020-03-08 17:21:45 +0700

    moving private info. to the dev.js

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
 const express = require('express');
 const app = express();
 const mongooose = require('mongoose');
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+const { User } = require('./model/user');
 
-mongooose.connect('mongodb+srv://aristo85:123456Abc@cluster0-ehted.mongodb.net/test?retryWrites=true&w=majority',
-    {useNewUrlParser: true}
-    ).then(() => console.log('DB is connected')).catch(err => console.error(err));
+const config = require('./config/key');
+
+mongooose.connect(config.mongoURI, {useNewUrlParser: true}
+).then(() => console.log('DB is connected')).catch(err => console.error(err));
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('hello world');
+})
+
+app.post('/api/users/register', (req, res) => {
+    const user = new User(req.body);
+    user.save((err, userData) => {
+        if(err) return res.json({success: false, err});
+    })
+    return res.status(200).json({success: true});
 })
 
 app.listen(5000);
